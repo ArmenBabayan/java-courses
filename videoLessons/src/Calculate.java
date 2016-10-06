c2e1a989e6d0cfc8929d8da695f98ef8520d5b75
Armen <armengagikovich93@gmail.com>
2016-10-07 01:09:18 +0400

    finish

--- videoLessons/src/Calculate.java
+++ videoLessons/src/Calculate.java
@@ -3,6 +3,11 @@
  */
 public class Calculate {
     public static void main(String[] args) {
+        int first =Integer.valueOf(args[0]);
+        int second=Integer.valueOf(args[1]);
+        int sum;
         System.out.println("Calculate.. ");
+        System.out.println(sum=first+second);
+        System.out.println(sum=first*second);
     }
 }
